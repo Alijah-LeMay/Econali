e0a0cdfdef6fd42f8bc0d2faa5708c8ba1bfa40e
CoderBoy4715 <alijahlemay12@gmail.com>
2022-11-05 23:55:12 -0700

    built

---
+++ frontend/build/precache-manifest.763a0f8c2a73f9d2e8c6005ff69614e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecca1d804d19abc2330ac38acb28925c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71445599003b04a7500a",
+    "url": "/static/css/main.ae6b5ca5.chunk.css"
+  },
+  {
+    "revision": "5c492e240867c955312d",
+    "url": "/static/js/2.7ad087c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7ad087c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71445599003b04a7500a",
+    "url": "/static/js/main.43de45f1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
